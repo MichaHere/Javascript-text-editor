44851a08c06598237b54c94b1bad189c3f26c84b
Micha <micha@michahere.com>
2025-03-13 22:15:39 +0100

    Apply stash changes

--- script/textEditor/Delta.js
+++ script/textEditor/Delta.js
@@ -1,20 +1,7 @@
-function removeAt(index, count, string) {
-    return string.slice(0, index) + string.slice(index + count);
+function insert_text_at (text, at, insert) {
+    return text.substring(0, at) + insert + text.substring(at);
 }
 
-function get_closest_integer(goal, array) {
-    return array.reduce((previous, current) => {
-        return (current > goal) ? previous : current
-    })
-}
-
-/**
- * @class
- * @param {Array} operations - The operations that should applied to the created Delta. 
- * @param {JSON} options - The options to use in the Delta, including formats and block types to use. 
- * @description A state element that can be used to transform operations into HTML format. 
- * The order of the operations does not matter. 
- */
 class Delta {
     constructor(operations = [], options) {
         this.operations = operations;
@@ -49,34 +36,65 @@ class Delta {
         }
 
         // Overwrite default options
-        if (options) this.options = { ...this.options, ...options };
-
-        this.__init__();
+        this.options = { ...this.options, ...options };
     }
 
-    __init__() {
-        this.clean();
-    }
+    insert_text(text, position, format = []) {
+        // TODO: Add functionality 
 
-    sort(delta = this) {
-        delta.operations.sort((a, b) => {
-            return parseFloat(a.position) - parseFloat(b.position);
+        var node_type = "insert";
+        var content_type = "text";
+        var last = this.operations[this.operations.length - 1];
+
+        if (
+            last.type === node_type &&
+            last.text.length + last.position === position
+        ) {
+            last.text += text;
+            return;
+        }
+
+        this.operations.push({
+            "type": node_type,
+            "position": position,
+            "content_type": content_type,
+            "text": text,
+            "format": format
         })
     }
 
-    clean(delta = this) {
-        this.sort(delta);
+    insert_break(type, position) {
+        // TODO: Add functionality 
+    }
 
-        for (let i = 0; i < delta.operations.length; i++) {
-            let current = delta.operations[i];
-            let previous = delta.operations[i-1];
+    delete(count, position) {
+        // TODO: Add functionality 
+    }
 
-            if (current.type !== "insert") continue;
-            if (current.content_type !== "text") continue;
+    get HTML() {
+        var HTML = "";
+        var content = this.content;
 
-            if (!current.text) {
-                delta.operations.splice(i, 1);
-                i--;
+        var outer_tag = "";
+        var inner_tag = "";
+
+        for (let i = 0; i < content.length; i++) {
+            let element = content[i];
+
+            if (element.content_type === "text") {
+                let starting_tag = "";
+                let ending_tag = "";
+
+                for (let i = 0; i < element.format.length; i++) {
+                    let format = element.format[i];
+                    let format_data = this.options.formats[format];
+
+                    starting_tag = starting_tag + `<${format_data.tag}>`;
+                    ending_tag = `</${format_data.tag}>` + ending_tag;
+                }
+
+                HTML += starting_tag + element.text + ending_tag;
+                
                 continue;
             }
             
@@ -92,35 +110,57 @@ class Delta {
                 i--;
                 continue;
             }
+
+            this.#warning("get HTML", "invalid content type encountered");
         }
 
-        this.recalculate_positions();
-    } 
+        HTML += (inner_tag) ? `</${inner_tag}>` : "";
+        HTML += (outer_tag) ? `</${outer_tag}>` : "";
 
-    recalculate_positions(ordered_delta = this) {
-        let current_position = 0;
-
-        for (let i = 0; i < ordered_delta.operations.length; i++) {
-            let operation = ordered_delta.operations[i];
-            
-            if (operation.type !== "insert") continue;
-            
-            operation.position = current_position;
-
-            if (operation.content_type === "text") {
-                current_position += operation.text.length
-            }
-
-            if (operation.content_type === "break") {
-                current_position++
-            }
-        }
-
-        ordered_delta.length = current_position;
+        return HTML;
     }
 
-    apply_operations(delta = this) {
-        this.sort(delta);
+    get content() {
+        return this.#operations_to_content();
+    }
+
+    #operations_to_content(operations = this.operations) {
+        var content = this.#apply_all_delete_operations(operations);
+        // TODO: Make sure to append a break if the content is empty 
+        // FIX: Make breaks work even if they are not appended at the end of the text elements 
+        content = this.#merge_sequential_content(content);
+        content = this.#recalculate_positions(content);
+        return content;
+    }
+
+    #sort_operations(operations = this.operations) {
+        var sorted_operations = structuredClone(operations);
+
+        sorted_operations.sort((a, b) => {
+            let result = parseFloat(a.position) - parseFloat(b.position);
+            if (result === 0 && a.type !== b.type) {
+                result = (a.type === "insert") ? -1 : 1;
+            }
+            return result;
+        })
+
+        return sorted_operations;
+    }
+
+    #apply_all_delete_operations(operations = this.operations) {
+        operations = this.#sort_operations(operations);
+
+        // get the position of the last delete operation
+        function last_delete_operation_index() {
+            let reverse_index = structuredClone(operations).reverse().findIndex(operation => {
+                if (operation.type === "delete") {
+                    return operation;
+                }
+            })
+
+            if (reverse_index < 0) return -1;
+            return operations.length - 1 - reverse_index;
+        }
 
         var delete_operation_indexes = this.filter_operations("delete", delta);
 
@@ -159,28 +199,32 @@ class Delta {
                 total_delete_count = total_delete_count - delete_count;
             }
 
-            if (insert_operation.content_type === "break") {
-                delta.operations.splice(index, 1);
-                total_delete_count--;
-                i--;
-            }
         }
-
-        delta.operations.splice(operation_index, 1);
     }
 
-    get_HTML_tags(tags) {
-        var start = "";
-        var end = "";
+    #merge_sequential_content(content = this.content) {
+        content = structuredClone(content);
 
-        for (let tag of tags) {
-            start = start + `<${tag}>`;
-            end = `</${tag}>` + end;
-        }
+        for (let i = 0; i < content.length; i++) {
+            let current = content[i];
+            let next = content[i + 1];
 
-        return {
-            "start": start,
-            "end": end,
+            if (!current || !next) continue;
+            if (current.type !== "insert") continue;
+            if (next.type !== "insert") { i++; continue; }
+            if (current.content_type !== "text" || next.content_type !== "text") continue;
+
+            // Check if formats are the same
+            if (!(current.format.length === next.format.length &&
+                current.format.every(current_format => {
+                    return next.format.includes(current_format);
+                })
+            )) continue;
+            
+            // FIX: Maybe use a local position 
+            console.log(current.text, next.position, next.text);
+            current.text = insert_text_at(current.text, next.position, next.text);
+            content.splice(1 + i--, 1);
         }
     }
 
