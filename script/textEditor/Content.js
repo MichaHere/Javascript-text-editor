37262407af64f54eb01a9cbc2ee5dab1b9ff18de
Micha <67926505+MichaHere@users.noreply.github.com>
2025-04-15 18:29:59 +0200

    Added functionality to the split_operation function and added get_plain_text function for testing purposes

--- script/textEditor/Content.js
+++ script/textEditor/Content.js
@@ -1,5 +1,9 @@
 import Delta from "./Delta.js";
 
+function insert_text_at (text, at, insert) {
+    return text.substring(0, at) + insert + text.substring(at);
+}
+
 class Content extends Delta {
     constructor(operations = [], options = {}) {
         super(operations, options);
@@ -8,49 +12,71 @@ class Content extends Delta {
     get content() {
         var operations = this.oder_operations(this.operations);
         operations = this.make_queue(operations);
-
+        
         var content = operations;
         return content;
     }
 
     get HTML() {
-        return JSON.stringify(this.content);
+        console.log(this.content);
+        return this.get_plain_text(this.content);
     }
 
     make_queue(operations = this.operations) {
         var operations = structuredClone(operations);
         var position = 0;
 
-        let last_operation;
+        let last_text;
         for (let i = 0; i < operations.length; i++) {
             let operation = operations[i];
 
-            if (operation.type !== "insert") {
-                last_operation = operation;
-                continue;
-            }
+            if (operation.type !== "insert") continue;
 
             if (operation.content_type !== "text") {
                 position++;
-
-                last_operation = operation;
                 continue;
             }
 
             if (operation.position < position) {
-                // TODO: This should append the current operation to the previous one if they have the same type, 
-                // otherwise it should split the previous operation at the position of the current operation and reorder. 
+                if (this.has_same_format(operation, last_text)) {
+                    let insert_position = operation.position - last_text.position;
+                    last_text.text = insert_text_at(last_text.text, insert_position, operation.text);
+                    operations.splice(i, 1);
+                }
+
+                // Split the last operation, and append the current operation in the middle of the split
+                let split = this.split_operation(last_text, operation.position);
+                let last_text_index = operations.indexOf(last_text);
+                operations.splice(last_text_index, 1, split[0], operation, split[1]);
+                operations.splice(i, 1);
+                
             }
 
             position += operation.text.length;
-            last_operation = operation;
+            last_text = operation;
         }
 
         return operations;
     }
 
     split_operation(operation, position) {
-        // TODO: Add functionality
+        let a = structuredClone(operation);
+        let b = structuredClone(operation);
+
+        a.text = a.text.substring(0, position);
+        b.text = b.text.substring(position);
+
+        return [a, b];
+    }
+
+    has_same_format(a, b) {
+        if (a.format.length !== b.format.length) return false;
+
+        if (!a.format.every(a_format => {
+            return b.format.includes(a_format);
+        })) return false;
+
+        return true;
     }
 
     oder_operations(operations = this.operations) {
@@ -65,6 +91,23 @@ class Content extends Delta {
         })
 
         return odered_operations;
+    }
+
+
+    get_plain_text(content = this.content) {
+        var text = "";
+
+        for (let i = 0; i < content.length; i++) {
+            let element = content[i];
+            if (element.type !== "insert") continue;
+            if (element.content_type !== "text") {
+                text += "\n";
+                continue;
+            };
+            text += element.text;
+        }
+
+        return text;
     }
 }
 
