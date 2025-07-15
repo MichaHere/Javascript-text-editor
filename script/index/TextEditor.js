77a37f76bc8838b8c20635ad15a74e939d64ed25
Micha <67926505+MichaHere@users.noreply.github.com>
2025-07-15 22:06:21 +0200

    Added a temporary test case

--- script/index/TextEditor.js
+++ script/index/TextEditor.js
@@ -7,4 +7,22 @@ var text_editor_elements = document.getElementsByClassName("text_editor");
 for (let i = 0; i < text_editor_elements.length; i++) {
     let text_editor_element = text_editor_elements[i];
     let text_editor = new TextEditor(text_editor_element, Format);
+
+    text_editor.state.commands = [
+        {
+            "type": "insert",
+            "text": "This a test",
+            "delete_count": 0,
+            "attributes": [],
+            "position": 0
+        },
+        {
+            "type": "insert",
+            "text": "is ",
+            "delete_count": 0,
+            "attributes": [],
+            "position": 5
+        }
+    ]
+    text_editor.update();
 }
