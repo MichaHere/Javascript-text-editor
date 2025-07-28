3d9b1b1ff9555d0befd92000e3b4cd3bac3f7032
Micha <67926505+MichaHere@users.noreply.github.com>
2025-07-28 12:03:31 +0200

    Add testing code for the selection get method

--- script/index/TextEditor.js
+++ script/index/TextEditor.js
@@ -8,28 +8,9 @@ for (let i = 0; i < text_editor_elements.length; i++) {
     let text_editor_element = text_editor_elements[i];
     let text_editor = new TextEditor(text_editor_element, Format);
 
-    text_editor.state.commands = [
-            {
-            "type": "insert",
-            "text": "This was a test",
-            "delete_count": 0,
-            "format": {
-            "block": "P",
-            "inline": []
-            },
-            "position": 0
-        },
-        {
-            "type": "insert",
-            "text": "is ",
-            "delete_count": 4,
-            "format": {
-            "block": "P",
-            "inline": []
-            },
-            "position": 5
-        }
-    ]
-
-    text_editor.updateContent();
+    // NOTE: test new position method
+    text_editor_element.innerHTML = '<p>This is the first line in the testing data. <br>This is a line break in the first line. <br></p><p>This is the second line. <br></p><p>And this the third. <br></p>'
+    text_editor_element.addEventListener('click', e => {
+        console.log(text_editor.selection.get());
+    })
 }
