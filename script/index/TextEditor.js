96b53fae03b1894f1824a4890d8103dfef4c878e
Micha <67926505+MichaHere@users.noreply.github.com>
2025-07-28 13:42:20 +0200

    Added extra testing for formatting

--- script/index/TextEditor.js
+++ script/index/TextEditor.js
@@ -9,7 +9,7 @@ for (let i = 0; i < text_editor_elements.length; i++) {
     let text_editor = new TextEditor(text_editor_element, Format);
 
     // NOTE: test new position method
-    text_editor_element.innerHTML = '<p>This is the first line in the testing data. <br>This is a line break in the first line. <br></p><p>This is the second line. <br></p><p>And this the third. <br></p>'
+    text_editor_element.innerHTML = '<p>This is the <strong><i>first</i> line</strong> in the testing data. <br>This is a line break in the <strong><i>first</i> line</strong>. <br></p><p>This is the <strong>second line</strong>. <br></p><p>And this the third. <br></p>'
     text_editor_element.addEventListener('click', e => {
         console.log(text_editor.selection.get());
     })
