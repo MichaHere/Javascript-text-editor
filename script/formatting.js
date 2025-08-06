995b81c3420bb4f2a651127c4db4a70ab8732e79
Micha <67926505+MichaHere@users.noreply.github.com>
2025-08-06 17:42:38 +0200

    Make paragraph breaks work (kind of)

--- script/formatting.js
+++ script/formatting.js
@@ -1,7 +1,7 @@
 class Format {
     constructor() {
         this.block = {
-            P: "\n",
+            P: "\r",
             BR: "\n",
         }
 
@@ -17,26 +17,36 @@ class Format {
         for (let i = 0; i < content.length; i++) {
             let element = content[i];
 
-            if (!buffer.firstChild) buffer.append(document.createElement(element.format.block));
+            if (!buffer.firstChild) {
+                buffer.append(document.createElement(element.format.block));
+            }
+
             if (buffer.firstChild.nodeName !== element.format.block.toUpperCase()) {
                 buffer.firstChild.append(document.createElement("br"));
                 html.append(buffer);
                 buffer.append(document.createElement(element.format.block));
             }
 
-            buffer.firstChild.append(this.element_to_html(element));
+            let paragraphs = element.text.split(this.block.P);
+
+            for (let i = 0; i < paragraphs.length; i++) {
+                buffer.firstChild.append(this.text_to_html(paragraphs[i], element.format));
+                buffer.firstChild.append(document.createElement("br"));
+                html.append(buffer);
+                buffer.append(document.createElement(element.format.block));
+            }
         }
         
-        buffer.firstChild.append(document.createElement("br"));
-        html.append(buffer);
+        // buffer.firstChild.append(document.createElement("br"));
+        // html.append(buffer);
 
         return html.innerHTML;
     }
 
-    element_to_html(element) {
+    text_to_html(text, format) {
         // TODO: Add proper inline format handling
         var html = new DocumentFragment();
-        var text_nodes = element.text.split("\n");
+        var text_nodes = text.split(this.block.BR);
         
         for (let i = 0; i < text_nodes.length - 1; i++) {
             let text_node = text_nodes[i];
